506e6b5ab214ba689bad1a9e1e9d3ef6c089c2e1
willian95 <rodriguezwillian95@gmail.com>
2022-01-17 12:58:32 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  
+  <div class='embed-container' data-page-width='494' data-page-height='640' id='ypembedcontainer' ><iframe   src="https://www.yumpu.com/es/embed/view/QmxeK63dAi1HXzYv" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe><br><a href="https://www.yumpu.com/es/document/view/66220732/documentacion-de-consumo-de-api-maando-para-crear-solicitudes-de-envio-de-paquetes" title="Documentación de consumo de API Maando para crear solicitudes de envió de paquetes" target="_blank">Documentación de consumo de API Maando para crear solicitudes de envió de paquetes</a></div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+  .embed-container { position: relative; padding-bottom:56.25%; height:0; overflow: hidden; max-width: 100%!important; } 
+  .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%;}
+  </style>
